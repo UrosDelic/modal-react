2e90268d919138bc29409692564fbf7f1159ee05
uros_delic <uros.delic@quantox.com>
2021-07-07 10:02:38 +0200

    fonts

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Modal from "./modal/Modal";
 const MainPage = () => {
+  const modalInfoText =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
   const [isModal, setModal] = useState(false);
 
   const openModal = () => {
@@ -18,7 +20,7 @@ const MainPage = () => {
       <Modal
         open={isModal}
         headline={"Modal Headline prop"}
-        modalText={"Modal sample text prop"}
+        modalText={modalInfoText}
         onClick={closeModal}
       >
         test modal child

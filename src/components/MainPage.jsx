6e34fd313ea7bfa7f9286f275bc7f8996c6d0717
Uros47 <urosdelic89@gmail.com>
2021-07-11 19:38:59 +0200

    useEffect added

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "./modal/Modal";
 import ConfirmationDialog from "./Confirmation-dialog/ConfirmationDialog";
 
 const MainPage = () => {
   const [isModal, setModal] = useState(false);
 
-  if (isModal === true) {
-    document.body.style.overflow = "hidden";
-  } else document.body.style.overflow = "visible";
+  useEffect(() => {
+    if (isModal === true) {
+      document.body.style.overflow = "hidden";
+    } else document.body.style.overflow = "unset";
+  }, [isModal]);
 
   const openModal = () => {
     setModal(true);
@@ -21,7 +23,7 @@ const MainPage = () => {
     <div>
       <button onClick={openModal}>Open Modal</button>
       {isModal && (
-        <Modal close={closeModal}>
+        <Modal close={closeModal} preventCloseOnClickOutside>
           <p>Confirmation Dialog</p>
           <ConfirmationDialog acceptText={"Accept"} declineText={"Decline"}></ConfirmationDialog>
         </Modal>
